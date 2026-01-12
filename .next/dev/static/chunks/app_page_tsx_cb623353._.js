9867731568a0405579238b8e711b59faae96d4d0
umarnaxir <erumarnazir@gmail.com>
2026-01-12 16:31:22 +0530

    new version:

--- .next/dev/static/chunks/app_page_tsx_cb623353._.js
+++ .next/dev/static/chunks/app_page_tsx_cb623353._.js
@@ -1,9 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__db8f3e92._.css",
-  "static/chunks/node_modules_0c1dd929._.js",
-  "static/chunks/app_147bd7c8._.js"
+  "static/chunks/_46a74d9e._.js",
+  "static/chunks/node_modules_ed4953bd._.js"
 ],
     source: "dynamic"
 });
