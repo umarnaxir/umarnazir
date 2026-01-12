08d9c0876604db46e1e9a98eb29306a5df9ac61a
umarnaxir <erumarnazir@gmail.com>
2026-01-12 18:07:46 +0530

    -

---
+++ .next/dev/server/app/api/contact/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/contact/route.js")
+R.c("server/chunks/node_modules_@react-email_render_dist_node_index_mjs_ce032ab5._.js")
+R.c("server/chunks/node_modules_4f981528._.js")
+R.c("server/chunks/[root-of-the-server]__110818ff._.js")
+R.c("server/chunks/_next-internal_server_app_api_contact_route_actions_0bce5875.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/contact/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/contact/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
